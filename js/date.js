b52e38a5200675d0d0621d5dc8b882d6db193c6d
tesuri <tesuri@gmail.com>
2014-06-05 05:36:01 +0000

    初期バージョン

---
+++ js/date.js
@@ -0,0 +1,120 @@
+/*global window, document, tizen, setTimeout */
+/*jslint plusplus: true*/
+
+window.requestAnimationFrame = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+        'use strict';
+        window.setTimeout(callback, 1000 / 60);
+    };
+    
+function date() {
+    'use strict';
+
+    //Import the current time
+    //noinspection JSUnusedAssignment
+    var date = new Date(),
+        day = date.getDate(),
+        month = date.getMonth()
+    
+    switch(day){
+    case 1:
+    case 21:
+    case 31:
+        $(".d01").css("color","#fff");break;
+    case 2:
+    case 22:
+        $(".d02").css("color","#fff");break;
+    case 3:
+    case 23:
+        $(".d03").css("color","#fff");break;
+    case 4:
+    case 24:
+        $(".d04").css("color","#fff");break;
+    case 5:
+    case 25:
+        $(".d05").css("color","#fff");break;
+    case 6:
+    case 26:
+        $(".d06").css("color","#fff");break;
+    case 7:
+    case 27:
+        $(".d07").css("color","#fff");break;
+    case 8:
+    case 28:
+        $(".d08").css("color","#fff");break;
+    case 9:
+    case 29:
+        $(".d09").css("color","#fff");break;
+    case 10:
+        $(".d10").css("color","#fff");break;
+    case 11:
+        $(".d11").css("color","#fff");break;
+    case 12:
+        $(".d12").css("color","#fff");break;
+    case 13:
+        $(".d13").css("color","#fff");break;
+    case 14:
+        $(".d14").css("color","#fff");break;
+    case 15:
+        $(".d15").css("color","#fff");break;
+    case 16:
+        $(".d16").css("color","#fff");break;
+    case 17:
+        $(".d17").css("color","#fff");break;
+    case 18:
+        $(".d18").css("color","#fff");break;
+    case 19:
+        $(".d19").css("color","#fff");break;
+    case 20:
+        $(".d20").css("color","#fff");break;
+    case 30:
+        $(".d20").css("color","#fff");break;
+    }
+    if(day===31){
+    	$(".d3x").css("color","#fff");
+    }else if(day>20 && day <30){
+    	$(".d2x").css("color","#fff");
+    }
+
+    switch(month){
+        case 0:
+            $(".jan").css("color","#fff");break;
+        case 1:
+        	$(".feb").css("color","#fff");break;
+        case 2:
+        	$(".mar").css("color","#fff");break;
+        case 3:
+        	$(".apr").css("color","#fff");break;
+        case 4:
+        	$(".may").css("color","#fff");break;
+        case 5:
+        	$(".jun").css("color","#fff");break;
+        case 6:
+        	$(".jul").css("color","#fff");break;
+        case 7:
+        	$(".aug").css("color","#fff");break;
+        case 8:
+        	$(".sep").css("color","#fff");break;
+        case 9:
+        	$(".oct").css("color","#fff");break;
+        case 10:
+        	$(".nov").css("color","#fff");break;
+        case 11:
+        	$(".dec").css("color","#fff");break;
+    }
+
+}
+
+window.onload = function () {
+    'use strict';
+
+    window.addEventListener('touchend', function (e) {
+        window.location.href="time.html";
+    },false);
+
+    window.requestAnimationFrame(date);
+};
---
+++ js/time.js
@@ -0,0 +1,190 @@
+/*global window, document, tizen, setTimeout */
+/*jslint plusplus: true*/
+
+var five, ten, quarter, twenty, half,  to, past, 
+    t1, t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,
+    am, pm, oclock;
+
+window.requestAnimationFrame = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+        'use strict';
+        window.setTimeout(callback, 1000 / 60);
+    };
+    
+function initCSS(){
+    t1.css("color","#444");
+    t2.css("color","#444");
+    t3.css("color","#444");
+    t4.css("color","#444");
+    t5.css("color","#444");
+    t6.css("color","#444");
+    t7.css("color","#444");
+    t8.css("color","#444");
+    t9.css("color","#444");
+    t10.css("color","#444");
+    t11.css("color","#444");
+    t12.css("color","#444");
+    five.css("color","#444");
+    ten.css("color","#444");
+    quarter.css("color","#444");
+    twenty.css("color","#444");
+    half.css("color","#444");
+    to.css("color","#444");
+    past.css("color","#444");
+    am.css("color","#444");
+    pm.css("color","#444");
+    oclock.css("color","#444");
+}
+
+function watch() {
+    'use strict';
+
+    //Import the current time
+    //noinspection JSUnusedAssignment
+    var date = new Date(),
+        hours = date.getHours(),
+        minutes = date.getMinutes(),
+        nextMove = 1000 - date.getMilliseconds();
+    
+    initCSS();
+    
+    switch(true){
+    case (minutes > 57 ):
+        oclock.css("color","#fff");
+        hours++;
+        break;
+    case ( minutes > 52 ):
+        to.css("color","#fff");
+        five.css("color","#fff");
+        hours++;
+        break;
+    case ( minutes > 47 ):
+        to.css("color","#fff");
+        ten.css("color","#fff");
+        hours++;
+        break;
+    case ( minutes > 42 ):
+        to.css("color","#fff");
+        quarter.css("color","#fff");
+        hours++;
+        break;
+    case ( minutes > 37):
+        to.css("color","#fff");
+        twenty.css("color","#fff");
+        hours++;
+        break;
+    case ( minutes > 32):
+        to.css("color","#fff");
+        twenty.css("color","#fff");
+        five.css("color","#fff");
+        hours++;
+        break;
+    case ( minutes > 27):
+        past.css("color","#fff");
+        half.css("color","#fff");
+        break;
+    case ( minutes > 22):
+        past.css("color","#fff");
+        twenty.css("color","#fff");
+        five.css("color","#fff");
+        break;
+    case ( minutes > 17):
+        past.css("color","#fff");
+        twenty.css("color","#fff");
+        break;
+    case ( minutes > 12):
+        quarter.css("color","#fff");
+        past.css("color","#fff");
+        break;
+    case ( minutes > 7):
+        ten.css("color","#fff");
+        past.css("color","#fff");
+        break;
+    case ( minutes > 2):
+        five.css("color","#fff");
+        past.css("color","#fff");
+        break;
+    default:
+        oclock.css("color","#fff");
+        break;
+    }
+    if(hours === 24) {
+        hours = hours - 12;
+        am.css("color","#fff");
+    } if( hours > 11 ) {
+        hours = hours - 12;
+        pm.css("color","#fff");
+    } else {
+        am.css("color","#fff");
+    }
+
+    switch(hours){
+        case 0:
+        case 12:
+            t12.css("color","#fff");break;
+        case 1:
+            t1.css("color","#fff");break;
+        case 2:
+            t2.css("color","#fff");break;
+        case 3:
+            t3.css("color","#fff");break;
+        case 4:
+            t4.css("color","#fff");break;
+        case 5:
+            t5.css("color","#fff");break;
+        case 6:
+            t6.css("color","#fff");break;
+        case 7:
+            t7.css("color","#fff");break;
+        case 8:
+            t8.css("color","#fff");break;
+        case 9:
+            t9.css("color","#fff");break;
+        case 10:
+            t10.css("color","#fff");break;
+        case 11:
+            t11.css("color","#fff");break;
+    }
+
+    setTimeout(function () {
+        window.requestAnimationFrame(watch);
+    }, nextMove);
+}
+
+window.onload = function () {
+    'use strict';
+
+    window.addEventListener('touchend', function (e) {
+        window.location.href="date.html";
+    },false);
+    
+    five = $(".five");
+    ten = $(".ten");
+    quarter= $(".quarter");
+    twenty= $(".twenty");
+    half = $(".half");
+    to = $(".to");
+    past = $(".past");
+
+    t1 = $(".t1");
+    t2 = $(".t2");
+    t3 = $(".t3");
+    t4 = $(".t4");
+    t5 = $(".t5");
+    t6 = $(".t6");
+    t7 = $(".t7");
+    t8 = $(".t8");
+    t9 = $(".t9");
+    t10 = $(".t10");
+    t11 = $(".t11");
+    t12 = $(".t12");
+    am= $(".am");
+    pm= $(".pm");
+    oclock = $(".oclock");
+
+    window.requestAnimationFrame(watch);
+};
